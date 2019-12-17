0233052d4e312d1caca218080f19a94617142db7
wangxingkang <156148958@qq.com>
2019-12-17 15:06:02 +0800

    feat: 代码优化

--- pansy.config.ts
+++ pansy.config.ts
@@ -1,12 +1,20 @@
 import { Config } from '@walrus/pansy';
 
+const globBy = require('globby');
+
 const config: Config = {
-  output: {
-    format: ['cjs', 'es', 'umd', 'umd-min'],
-    moduleName: 'pansyHooks',
-    sourceMap: true
+  input: {
+    index: 'src/use-modal/index.ts',
+    'use-modal': 'src/use-modal/index.ts',
+    'use-toggle': 'src/use-toggle/index.ts'
   },
-  externals: [...Object.keys(require('./package').dependencies)]
+  output: {
+    format: ['cjs', 'es']
+  },
+  externals: [
+    ...Object.keys(require('./package').dependencies),
+    ...Object.keys(require('./package').peerDependencies)
+  ]
 };
 
 export default config;
