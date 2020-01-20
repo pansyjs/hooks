c5030d35c9712c1f9ed5e8a4202ef9c5d8d2d6df
wangxingkang <wangxingkang@sensoro.com>
2020-01-20 11:03:59 +0800

    feat: add useLocalStorage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { default as useModal } from './use-modal';
 export { default as useToggle } from './use-toggle';
 export { default as useFullscreen } from './use-fullscreen';
+export { default as useWindowSize  } from './use-window-size';
+export { default as useLocalStorage } from './use-local-storage/';
+
+export { UseModalResult } from './use-modal';
