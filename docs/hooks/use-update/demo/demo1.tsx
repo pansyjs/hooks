0e65cd1bb25975f98114f3649ce8cab72a2d28fd
kang <wang_xingkang@qq.com>
2020-06-13 23:56:07 +0800

    feat(use-update): add useUpdate

---
+++ docs/hooks/use-update/demo/demo1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import { useUpdate } from '@pansy/hooks';
+
+export default () => {
+  const update = useUpdate();
+
+  return (
+    <>
+      <div>Time: {Date.now()}</div>
+      <Button onClick={update} style={{ marginTop: 16 }}>update</Button>
+    </>
+  );
+};
