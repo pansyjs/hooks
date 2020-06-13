c45c961f410c5f0a0248da36f88a1f97d6f061ee
kang <wang_xingkang@qq.com>
2020-06-13 23:56:52 +0800

    feat: add useUpdateEffect useUpdateLayoutEffect

---
+++ docs/hooks/use-update-effect/demo/demo1.tsx
@@ -0,0 +1,36 @@
+/**
+ * title: 基础使用
+ * desc: 使用上与 useEffect 完全相同，只是它忽略了首次渲染，且只在依赖项更新时运行。
+ */
+import React, { useLayoutEffect, useState } from 'react';
+import { Button } from 'antd';
+import { useUpdateEffect } from '@pansy/hooks';
+
+export default () => {
+  const [count, setCount] = useState(0);
+  const [effectCount, setEffectCount] = useState(0);
+  const [updateEffectCount, setUpdateEffectCount] = useState(0);
+
+  useLayoutEffect(() => {
+    setEffectCount(c => c + 1);
+  }, [count]);
+
+  useUpdateEffect(() => {
+    setUpdateEffectCount(c => c + 1);
+    return () => {
+      // do something
+    };
+  }, [count]); // you can include deps array if necessary
+
+  return (
+    <div>
+      <p>effectCount: {effectCount}</p>
+      <p>updateEffectCount: {updateEffectCount}</p>
+      <p>
+        <Button onClick={() => setCount(c => c + 1)}>
+          reRender
+        </Button>
+      </p>
+    </div>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,10 @@ export { default as usePosition } from './use-position';
 export { default as useWindowSize  } from './use-window-size';
 export { default as useLocalStorage } from './use-local-storage';
 
+export { default as useMount } from './use-mount';
+export { default as useUpdate } from './use-update';
 export { default as useUnmount } from './use-unmount';
+export { default as useUpdateEffect } from './use-update-effect';
+export { default as useUpdateLayoutEffect } from './use-update-layout-effect';
+
+export { default as usePersistFn } from './use-persist-fn';
